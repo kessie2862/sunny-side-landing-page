7b60049342a11eb77265836d7566f7caa18f1ca7
kessie <kayp3477@gmail.com>
2022-05-03 16:28:42 +0000

    Added some comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Toggle the navbar on smaller viewport
 var menu = document.querySelector(".hamburger");
 menu.addEventListener("click", function () {
   document.querySelector(".nav").classList.toggle("toggle");
